0ea8869c9e6ac3c9067ca34746c376ea328a82ae
Prabesh Shrestha <creatorworld329@gmail.com>
2021-02-20 08:55:15 +0545

    using sys

--- src/main.py
+++ src/main.py
@@ -2,14 +2,27 @@ from tensorflowfunction import *
 from bluetoothcommunication import *
 from tkinter import *
 import pyttsx3
+from threading import * 
+import sys
 
 rootWindow = Tk()
+rootWindow.iconphoto(False, PhotoImage(file = "src/pics/eye.png"))
 rootWindow.title("Safe eye")
-rootWindow.
+rootWindow.geometry("500x500")
 
 robotVoice = pyttsx3.init()
+
+
+def welcome():
+    robotVoice.say("Safe eye started")
+    robotVoice.runAndWait()
+
+welcomeThread = Thread(target=welcome).start()
+
+startButton = Button(rootWindow, text = "Start using camera").pack()
 
 
 
 
 rootWindow.mainloop()
+sys.exit()
